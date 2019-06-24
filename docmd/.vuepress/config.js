f4846597f3b6bccad05204f42c6b2bc7bb5a8ced
liuym123 <2777230076@qq.com>
2019-06-24 18:28:01 +0800

    'autocommit'

--- docmd/.vuepress/config.js
+++ docmd/.vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
   title: "vuepress-theme-reco",
   description: 'A simple and beautiful vuepress blog theme .',
   dest: 'docs',
+  base:'/reco/',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
