316ba60cebc3048771953b2a599ecd1df0a14954
mrzhoulichao <1751777388@qq.com>
2019-07-01 19:58:15 +0800

    update mongo入门

---
+++ docmd/.vuepress/components/SpecialLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="custom-container"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+
+<style lang="css">
+html,
+body,
+#app,
+.custom-container {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+.custom-container * {
+  margin: 0;
+  padding: 0;
+}
+</style>
+
--- docmd/.vuepress/config.js
+++ docmd/.vuepress/config.js
@@ -39,13 +39,12 @@ module.exports = {
         text: '关于我',
         icon: 'reco-message',
         items: [
-          // { text: 'NPM', link: 'https://www.npmjs.com/~reco_luan', icon: 'reco-npm' },
           {
             text: 'GitHub',
             link: 'https://github.com/Mrzhoulichao',
             icon: 'reco-github'
           },
-          // { text: '简书', link: 'https://www.jianshu.com/u/cd674a19515e', icon: 'reco-jianshu' },
+          { text: '关于我', link: '/about/about.html', icon: 'reco-account' },
           // { text: 'CSDN', link: 'https://blog.csdn.net/recoluan', icon: 'reco-csdn' },
           // { text: '博客圆', link: 'https://www.cnblogs.com/luanhewei/', icon: 'reco-bokeyuan' },
           // { text: 'WeChat', link: 'https://mp.weixin.qq.com/s/mXFqeUTegdvPliXknAAG_A', icon: 'reco-wechat' },
